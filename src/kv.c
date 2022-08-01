4ac504ecec7951fced83074d351889f768dee6ac
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-08-01 17:41:53 +0200

    fix build with clang-15

--- src/kv.c
+++ src/kv.c
@@ -11,9 +11,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "natsp.h"
 #include <ctype.h>
 
-#include "natsp.h"
 #include "kv.h"
 #include "mem.h"
 #include "util.h"
